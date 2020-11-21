c2581f7893f354f2c29dfc410ef4c9414cb8a6c1
Exp <theexp111@gmail.com>
2020-11-21 18:20:49 +0100

    RPC

--- AmongUsProxy/Handler.cs
+++ AmongUsProxy/Handler.cs
@@ -1,4 +1,5 @@
 ﻿using Impostor.Api.Net.Messages;
+using Impostor.Hazel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,27 @@ namespace AmongUsProxy
 	{
 		DataFlag = 1,
 		RPCFlag = 2,
+		Default = 3,
+		F4 = 4,
+		F5 = 5,
+		F6 = 6,
+		F7 = 7,
 	}
+
+	public enum RPCCallID : uint
+	{
+		CompleteTask = 0x1,
+		SetInfected = 0x3,
+		SetName = 0x6,
+		SetSkin = 0xa,
+		ReportBody = 0xb,
+		Chat = 0xd,
+		//SetScanner = 0xf,
+		MurderPlayer = 0xf,
+		SetPet = 0x11,
+		CheckForEndVoting = 0x17,
+	}
+
 	class Handler
 	{
 		public static void HandleGameData(bool toPlayer, IMessageReader packet)
@@ -22,16 +43,46 @@ namespace AmongUsProxy
 				Console.WriteLine($" - Target       {packet.ReadPackedInt32()}");
 			}
 
-			Console.WriteLine(HexUtils.HexDump(packet.Buffer.ToArray()));
+			//Console.WriteLine(HexUtils.HexDump(packet.Buffer.ToArray()));
 			while (packet.Position < packet.Length)
 			{
 				using var reader = packet.ReadMessage();
 				Console.WriteLine($" - Tag          {reader.Tag}");
-				switch (reader.Tag)
+				switch ((GameDataTag)reader.Tag)
 				{
+					case GameDataTag.RPCFlag:
+						var netID = reader.ReadPackedUInt32();
+						HandleRPC(netID, reader);
+						break;
 					default:
+						var readPos = reader.Offset + reader.Position;
+						Console.WriteLine(HexUtils.HexDump(packet.Buffer.ToList().
+							GetRange(readPos, reader.Length).ToArray()));
 						break;
 				}
+			}
+		}
+
+		public static void HandleRPC(uint netID, IMessageReader packet)
+		{
+			//TODO: get name from netid, default to netid if not yet seen
+			Console.WriteLine($" - NetID        {netID}");
+			var callID = packet.ReadByte();
+			Console.WriteLine($" - CallID       {callID}");
+			switch ((RPCCallID)callID)
+			{
+				case RPCCallID.Chat:
+					Console.WriteLine($"Message from {netID}: {packet.ReadString()}");
+					break;
+				case RPCCallID.MurderPlayer:
+					//TODO: read args
+					Console.WriteLine($"{netID} killed someone");
+					break;
+				default:
+					var readPos = packet.Offset + packet.Position;
+					Console.WriteLine(HexUtils.HexDump(packet.Buffer.ToList().
+							GetRange(readPos, packet.Length).ToArray()));
+					break;
 			}
 		}
 	}
